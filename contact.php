7f1a8d6f8f416baf67cbf8204aba54c916fe22ac
deshraj1889 <deshrajsaini786@yahoo.com>
2023-02-28 14:02:10 +0530

    fix: added the line in comment

--- contact.php
+++ contact.php
@@ -23,7 +23,7 @@ $mail_status = mail("info@propertyscanners.com",$subject,$body_message);
 
 if ($mail_status) { ?>
     <script language="javascript" type="text/javascript">
-        alert('Thank you for the message. We will contact you shortly.');
+        alert('Thank you for the message. We will contact you shortly...');
         window.location = 'index.html';
     </script>
 <?php
